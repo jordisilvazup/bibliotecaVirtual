c280c160af52357a297cd422f2b61e71e9aed9da
jordisilvazup <jordi.silva@zup.com.br>
2021-08-16 09:56:26 -0300

    setup do projeto

---
+++ src/main/java/br/com/zup/edu/biblioteca/BibliotecaVirtualApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaVirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaVirtualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/edu/biblioteca/BibliotecaVirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaVirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
